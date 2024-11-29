5eece24fc3f7756c85d79be139568738723841c5
f1-Makar <wu.front.end.dev@gmail.com>
2024-11-29 12:38:32 +0800

    创建vue项目 |添加eslint和prettier |添加UI库：Tailwind CSS和daisyuiUI

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <p>vue.app</p>
+  <button class="btn btn-primary">button</button>
+</template>
+
+<style scoped>
+
+</style>
