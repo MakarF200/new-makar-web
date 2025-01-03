dc784943f8df05eb148988bb774a2aa44dc8dd76
f1-Makar <wu.front.end.dev@gmail.com>
2025-01-03 20:09:19 +0800

    修改bug

--- src/views/contentview/ant-travel/about-ant-travel.vue
+++ src/views/contentview/ant-travel/about-ant-travel.vue
@@ -35,9 +35,11 @@ async function fetchMdContent(mdPath: string): Promise<string> {
  * @returns {string} 转换后的 HTML
  */
 function getMd(mdData: string): string {
+  // @ts-ignore
   return marked(mdData);  // 使用 marked 将 Markdown 转换为 HTML
 }
 </script>
+
 
 
 
